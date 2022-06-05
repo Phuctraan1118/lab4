3beb6a35632de3284400a6c8c7e2281d873200c0
Hieu <hieuphtse151459@fpt.edu.vn>
2022-06-06 00:12:57 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+        
+            $(document).ready(function(){
+                // Show the Modal on load
+                $("#myModal").modal("show");
+                  
+                // Hide the Modal
+                $("#btn").click(function(){
+                  $("#reserveform").modal("show");
+                });
+            });
